30be3cf792a23ce92dcc053b779a9123d861caa6
vaaghu <vignesh021102@gmail.com>
2024-02-19 03:28:11 +0530

    basic create and list read implemented with the validaiton pipe for it

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import {
+  IsEmail,
+  IsMobilePhone,
+  IsOptional,
+  IsString,
+} from '@nestjs/class-validator';
+
+class CreateUserDto {
+  @IsString()
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsMobilePhone()
+  @IsOptional()
+  phoneNumber?: string;
+}
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @Post()
+  async add(@Body() createUserDto: CreateUserDto) {
+    if (!createUserDto.email && !createUserDto.phoneNumber) {
+      throw new BadRequestException('Either email or phoneNumber must exist');
+    }
+    return await this.usersService.add(
+      createUserDto?.email,
+      createUserDto?.phoneNumber,
+    );
+  }
+
+  @Get()
+  async getAll() {
+    return await this.usersService.getAll();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async add(email: string, phoneNumber: string) {
+    console.log(email, phoneNumber);
+    return await prisma.contact.create({
+      data: { email: email, phoneNumber: phoneNumber },
+    });
+  }
+  async getAll() {
+    return await prisma.contact.findMany({
+      select: { id: true, email: true, phoneNumber: true },
+    });
+  }
+}
